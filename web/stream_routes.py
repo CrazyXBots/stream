7dfd60326137d9884ff6159437ee7d3bc99dfd7c
CrazyXBots <147149895+CrazyXBots@users.noreply.github.com>
2025-12-26 12:59:32 +0530

    Update stream_routes.py

--- web/stream_routes.py
+++ web/stream_routes.py
@@ -10,7 +10,7 @@ from aiohttp import web
 from aiohttp.http_exceptions import BadStatusLine
 
 from info import *
-from web.server import multi_clients, work_loads, web_server
+from web.server import multi_clients, work_loads
 from web.server.exceptions import FileNotFound, InvalidHash
 from web.utils.custom_dl import get_file_path
 from web.utils.render_template import render_page
